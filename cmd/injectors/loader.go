d63e847fcbb45d44968cfb4e43fea5dc67f9f79f
diegolopez-ing <diegolopez.coder@gmail.com>
2022-10-02 00:58:35 -0500

    Finalizando implementación5

--- cmd/injectors/loader.go
+++ cmd/injectors/loader.go
@@ -5,12 +5,9 @@ import (
 
 	"github.com/DiegoLopez-ing/api_rest/configs"
 	"github.com/DiegoLopez-ing/api_rest/database"
-<<<<<<< HEAD
-=======
 	"github.com/DiegoLopez-ing/api_rest/handlers"
 	"github.com/DiegoLopez-ing/api_rest/repos"
 	"github.com/DiegoLopez-ing/api_rest/services"
->>>>>>> 34b956835237401f626c95762799cba11afd2588
 	"gorm.io/gorm"
 )
 
